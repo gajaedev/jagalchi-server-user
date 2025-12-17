2d357c74a921e3b9da6e55d49abe311eefe70f01
leegunwoo <leegunwoo0325@gmail.com>
2025-12-17 19:30:37 +0900

    feat : 유저 도메인 클래스 작성

---
+++ src/main/java/gajeman/jagalchi/jagalchiserver/domain/UserRole.java
@@ -0,0 +1,7 @@
+package gajeman.jagalchi.jagalchiserver.domain;
+
+public enum UserRole {
+    STUDENT,
+    TEACHER,
+    ADMIN
+}
---
+++ src/main/java/gajeman/jagalchi/jagalchiserver/domain/Users.java
@@ -0,0 +1,62 @@
+package gajeman.jagalchi.jagalchiserver.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "tbl_users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    private String profileUrl;
+
+    private String text;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Builder
+    private Users(String email, String password, String profileUrl, String text) {
+        this.email = email;
+        this.password = password;
+        this.text = text;
+        this.role = UserRole.STUDENT;
+        this.profileUrl = profileUrl;
+    }
+
+    public Users from(String email, String password, String profileUrl, String text) {
+        return Users.builder()
+                .email(email)
+                .password(password)
+                .text(text)
+                .profileUrl(profileUrl)
+                .build();
+    }
+
+}
