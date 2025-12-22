c8e30abb63465b990e718b87ccefc9a24d0adff9
leegunwoo <leegunwoo0325@gmail.com>
2025-12-22 17:37:30 +0900

    feat(#1) : 컨트롤러 작성

---
+++ src/main/java/gajeman/jagalchi/jagalchiserver/presentation/AuthController.java
@@ -0,0 +1,25 @@
+package gajeman.jagalchi.jagalchiserver.presentation;
+
+import gajeman.jagalchi.jagalchiserver.application.auth.service.ChangePasswordCommand;
+import gajeman.jagalchi.jagalchiserver.presentation.user.dto.request.ChangePasswordRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class AuthController {
+
+    private final ChangePasswordCommand changePasswordCommand;
+
+    @PatchMapping("/auth/password-reset")
+    public void changePassword(
+            @RequestBody ChangePasswordRequest request
+    ) {
+        changePasswordCommand.changePassword(request);
+    }
+
+}
