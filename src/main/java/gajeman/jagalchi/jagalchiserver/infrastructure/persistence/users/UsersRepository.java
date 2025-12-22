95d6e0df6930d87d22a9d27aac5dd76ba45131ba
leegunwoo <leegunwoo0325@gmail.com>
2025-12-22 17:37:37 +0900

    feat(#1) : 오류 수정

---
+++ src/main/java/gajeman/jagalchi/jagalchiserver/infrastructure/persistence/users/UsersRepository.java
@@ -0,0 +1,10 @@
+package gajeman.jagalchi.jagalchiserver.infrastructure.persistence.users;
+
+import gajeman.jagalchi.jagalchiserver.domain.user.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByEmail(String email);
+}
