5abb2d885ea83ea5caf17cba3812853b84d6679a
leegunwoo <leegunwoo0325@gmail.com>
2025-12-23 17:26:18 +0900

    refactor(#1) : 중복 클래스 제거

--- src/main/java/gajeman/jagalchi/jagalchiserver/infrastructure/persistence/verification/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package gajeman.jagalchi.jagalchiserver.infrastructure.persistence.verification;
-
-import gajeman.jagalchi.jagalchiserver.domain.user.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users, Long> {
-}
