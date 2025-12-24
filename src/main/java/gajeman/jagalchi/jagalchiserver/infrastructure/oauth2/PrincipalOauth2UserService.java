b81b834d65a1de21d3bbbe69628a1751b5d07f53
leegunwoo <leegunwoo0325@gmail.com>
2025-12-24 10:29:44 +0900

    feat(#1) : 깃허브 오어스 구현

--- src/main/java/gajeman/jagalchi/jagalchiserver/infrastructure/oauth2/PrincipalOauth2UserService.java
+++ src/main/java/gajeman/jagalchi/jagalchiserver/infrastructure/oauth2/PrincipalOauth2UserService.java
@@ -2,6 +2,7 @@ package gajeman.jagalchi.jagalchiserver.infrastructure.oauth2;
 
 import gajeman.jagalchi.jagalchiserver.domain.user.Users;
 import gajeman.jagalchi.jagalchiserver.infrastructure.persistence.users.UsersRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -17,13 +18,15 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
     private final UsersRepository userRepository;
 
     @Override
+    @Transactional
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
         OAuth2User oauth2User = super.loadUser(userRequest);
 
-        String provideId = oauth2User.getAttribute("sub");
-        String email = oauth2User.getAttribute("email");
-        String name = oauth2User.getAttribute("name");
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        String email = getEmail(registrationId, oauth2User);
+        String name = getName(registrationId, oauth2User);
         String password = "OAuth2";
 
         Users user = userRepository.findByEmail(email)
@@ -37,7 +40,27 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
                         )
                 );
 
-            return new PrincipalDetails(user, oauth2User.getAttributes());
-        }
+        return new PrincipalDetails(user, oauth2User.getAttributes());
     }
 
+    private String getEmail(String registrationId, OAuth2User oauth2User) {
+        String email = oauth2User.getAttribute("email");
+
+        if (email == null && "github".equals(registrationId)) {
+            String login = oauth2User.getAttribute("login");
+            email = login + "@users.noreply.github.com";
+        }
+
+        return email;
+    }
+
+    private String getName(String registrationId, OAuth2User oauth2User) {
+        String name = oauth2User.getAttribute("name");
+
+        if (name == null && "github".equals(registrationId)) {
+            name = oauth2User.getAttribute("login");
+        }
+
+        return name;
+    }
+}
