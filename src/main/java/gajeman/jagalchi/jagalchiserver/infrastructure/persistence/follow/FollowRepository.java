fdcaf7ff274e8b3cd9ae1951fe152ea50d31e94f
leegunwoo <leegunwoo0325@gmail.com>
2025-12-28 01:26:42 +0900

    feat(#1) : 팔로우/팔로워 리스트 확인

--- src/main/java/gajeman/jagalchi/jagalchiserver/infrastructure/persistence/follow/FollowRepository.java
+++ src/main/java/gajeman/jagalchi/jagalchiserver/infrastructure/persistence/follow/FollowRepository.java
@@ -17,3 +17,4 @@ public interface FollowRepository extends JpaRepository<Follow, Long> {
 
     List<Follow> findByFollowing(Users following);
 }
+
--- src/main/java/gajeman/jagalchi/jagalchiserver/presentation/user/FollowController.java
+++ src/main/java/gajeman/jagalchi/jagalchiserver/presentation/user/FollowController.java
@@ -1,8 +1,11 @@
 package gajeman.jagalchi.jagalchiserver.presentation.user;
 
+import gajeman.jagalchi.jagalchiserver.application.follow.service.GetFollowerListQuery;
+import gajeman.jagalchi.jagalchiserver.application.follow.service.GetFollowingListQuery;
 import gajeman.jagalchi.jagalchiserver.application.follow.service.ToggleFollowCommand;
 import gajeman.jagalchi.jagalchiserver.domain.user.Users;
 import gajeman.jagalchi.jagalchiserver.presentation.user.dto.request.FollowToggleRequest;
+import gajeman.jagalchi.jagalchiserver.presentation.user.dto.response.FollowListResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +16,15 @@ import org.springframework.web.bind.annotation.*;
 public class FollowController {
 
     private final ToggleFollowCommand toggleFollowCommand;
+    private final GetFollowerListQuery getFollowerListQuery;
+    private final GetFollowingListQuery getFollowingListQuery;
 
+    /**
+     * 팔로우 거는 메서드
+     *
+     * @param name 유저 이름
+     * @param request boolean
+     */
     @PatchMapping("/{name}/follow")
     public void toggleFollow(
             @PathVariable String name,
@@ -25,6 +36,25 @@ public class FollowController {
                 name,
                 request.getToggle()
         );
+    }
+
+    /**
+     * 비밀번호 재설정용 이메일 인증 코드를 발송합니다.
+     *
+     * @param name 이메일 정보를 담은 요청 DTO
+     */
+    @GetMapping("/{name}/followers")
+    public FollowListResponse getFollowers(
+            @PathVariable String name
+    ){
+        return getFollowerListQuery.getFollowerList(name);
+    }
+
+    @GetMapping("/{name}/followings")
+    public FollowListResponse getFollowings(
+            @PathVariable String name
+    ){
+        return getFollowingListQuery.getFollowingList(name);
     }
 
 }
