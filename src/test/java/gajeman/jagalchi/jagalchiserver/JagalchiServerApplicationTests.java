90b7daaf1febc4033437ed1a7a3c51c64f8d0521
김현우 <162122452+HyunwooKiim@users.noreply.github.com>
2026-01-04 00:13:49 +0900

    refactor : remove unused contextLoads test method

--- src/test/java/gajeman/jagalchi/jagalchiserver/JagalchiServerApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package gajeman.jagalchi.jagalchiserver;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class JagalchiServerApplicationTests {
-}
